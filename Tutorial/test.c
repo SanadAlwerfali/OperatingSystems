764f1ac0399f2ff2c22cb7f94916bf3b06de5bdb
Sanad Alwerfali <smalwerfali@mun.ca>
2024-01-08 15:30:19 -0330

    This is the first commit which has the basic C tutorial

---
+++ Tutorial/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_something_for_me(const char *something){
+    printf("%s \n",something);
+};
+
+int main() {
+    char name[] = "Sanad";
+    char *name_pointer = &name[0];    
+    printf("Before changing the value %s \n", name_pointer);
+    print_something_for_me(name_pointer); 
+    strcpy(name_pointer, "Taha");
+    printf("After changing the value %s \n", name_pointer);
+    printf("Value of name after changing the value %s \n", name);
+    return 0;
+}
