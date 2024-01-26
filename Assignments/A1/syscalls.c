03c4f6f2a5c86939ceb4dfa16eaa2f149d8044b0
Sanad Alwerfali <smalwerfali@mun.ca>
2024-01-26 18:20:12 -0330

    fixed code structure

--- Assignments/A1/syscalls.c
+++ Assignments/A1/syscalls.c
@@ -8,25 +8,23 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void change_num (int* num) {
-    *num = 20;
-}
+
 void help_command() {
     char *message = "Usage:  syscalls <command> [arguments]\n";
-    write(2, message, strlen(message));
+    write(STDOUT_FILENO, message, strlen(message));
 }
 
 
 int read_command(char* path[]) {
-    if (path == NULL || *path == NULL) {
-        printf("missing argument\n");
+    if (path[0] == NULL) {
+        fprintf(stderr, "missing argument\n");
         return 1;
     }
 
     int file = open(*path, O_RDONLY);
 
     if (file == -1) {
-        printf("Failed to open %s\n", *path);
+        fprintf(stderr, "Failed to open %s\n", *path);
         return 1;
     }
 
@@ -35,27 +33,27 @@ int read_command(char* path[]) {
     int text = read(file, buff, sizeof(buff));
 
     if (text == -1) {
-        printf("Faild to read %s\n", *path);
+        fprintf(stderr, "Faild to read %s\n", *path);
+        close(file);
         return 1;
     }
 
     buff[text] = '\0';
-    write(2, buff, text);
+    write(STDOUT_FILENO, buff, text);
     close(file);
     return 0;
 }
 
 int write_command(char* path[], char* line_of_text[], int num_of_lines) {
     if (path == NULL || *path == NULL || line_of_text == NULL || *line_of_text == NULL) {
-        printf("missing argument\n");
+        fprintf(stderr, "missing argument\n");
         return 1;
     }
-
 
     int file = open(*path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     
     if (file == -1) {
-        printf("Failed to open %s\n", *path);
+        fprintf(stderr, "Failed to open %s\n", *path);
         return 1;
     }
     
@@ -64,38 +62,47 @@ int write_command(char* path[], char* line_of_text[], int num_of_lines) {
         if (line_of_text[i] != NULL) {
             int number_of_bytes = write(file, line_of_text[i], strlen(line_of_text[i]));
             if (number_of_bytes == -1) {
-                printf("Failed to write to %s\n", *path);
+                fprintf(stderr, "Failed to write to %s\n", *path);
                 return 1;
             }
             total_bytes += number_of_bytes;
 
-            number_of_bytes = write(file, " ", 1);
+            number_of_bytes = write(file, "\n", 1);
             if (number_of_bytes == -1) {
-                printf("Failed to write to %s\n", *path);
+                fprintf(stderr, "Failed to write to %s\n", *path);
                 return 1;
             }
             total_bytes += number_of_bytes;
         }
     }
 
-    printf("Wrote %d B\n", total_bytes);
+    fprintf(stdout, "Wrote %d B\n", total_bytes);
     close(file);
     return 0;
 }
 
 int mkdir_command(char* path[]) {
     mode_t mode = 0700;
-    int dir = mkdir(*path, mode);
-
-    if (dir == -1 && errno == EEXIST){
-        printf("%s already exists\n", *path);
-        return 1;
-    } 
-    if (dir == -1){
-        printf("Faild to create %s\n", *path);
-        return 1;
+    char *token;
+    
+    token = strtok(*path, "/");
+    char *previous = token;
+    
+    while( token != NULL ) {
+        char *current_token;
+        printf( " %s\n", token );
+        int dir = mkdir(token, mode);
+        if (dir == -1 && errno == EEXIST){
+            fprintf(stdout, "%s already exists\n", *path);
+            return 1;
+        } 
+        if (dir == -1){
+            fprintf(stdout, "Faild to create %s\n", *path);
+            return 1;
+        }
+        token = strtok(NULL, "/");
     }
-
+    
     return 0;
 }
 
@@ -104,7 +111,7 @@ int main (int argc, char* argv[]) {
     char* missing = "missing command\n";
 
     if (argc < 2) {
-        write(2, missing, strlen(missing));
+        write(STDERR_FILENO, missing, strlen(missing));
         return 1;
     };
 
@@ -113,13 +120,16 @@ int main (int argc, char* argv[]) {
     if (strcmp(command, "help") == 0) {
         help_command();
     } else if (strcmp(command, "read") == 0){
-        read_command(&argv[2]);
-    } else if (strcmp(command, "write") == 0){ 
-        write_command(&argv[2], &argv[3], argc - 3);
+        int command_response = read_command(&argv[2]);
+        if (command_response == 1) return 1;
+    } else if (strcmp(command, "write") == 0) { 
+        int write_command_response = write_command(&argv[2], &argv[3], argc - 3);
+        if (write_command_response == 1) return 1;
     } else if (strcmp(command, "mkdir") == 0){
-        mkdir_command(&argv[2]);
+        int mkdir_command_response = mkdir_command(&argv[2]);
+        if (mkdir_command_response == 1) return 1;
     } else {
-        write(2, invalid, strlen(invalid));
+        write(STDERR_FILENO, invalid, strlen(invalid));
         return 1;
     }
 
